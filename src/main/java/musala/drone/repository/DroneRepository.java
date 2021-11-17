053d26136e0c8ee0339cd9768b32983b38820247
Olusola Alao <alaoolusolae@gmail.com>
2021-11-17 03:49:49 +0100

    add drone repository

---
+++ src/main/java/musala/drone/repository/DroneRepository.java
@@ -0,0 +1,9 @@
+package musala.drone.repository;
+
+import musala.drone.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, String> {
+}
