49867de82e083b8e7ca2ebd152866e290c493012
Olusola Alao <alaoolusolae@gmail.com>
2021-11-18 05:08:14 +0100

    add find drone by serial number

--- src/main/java/musala/drone/repository/DroneRepository.java
+++ src/main/java/musala/drone/repository/DroneRepository.java
@@ -4,6 +4,11 @@ import musala.drone.model.Drone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface DroneRepository extends JpaRepository<Drone, String> {
+public interface DroneRepository extends JpaRepository<Drone, UUID> {
+    Optional <Drone> findBySerialNumber(String serialNumber);
 }
