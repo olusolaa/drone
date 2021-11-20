badc6c913cba933af281daa33aa9a405285f195b
Olusola Alao <alaoolusolae@gmail.com>
2021-11-20 01:44:00 +0100

    add find medication by code

---
+++ src/main/java/musala/repository/MedicationRepository.java
@@ -0,0 +1,13 @@
+package musala.repository;
+
+import musala.model.Drone;
+import musala.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    Optional<Medication> findByCode(String code);
+}
