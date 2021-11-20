e14e06d99bb9e6456680d7bc8a151eec3fbd1a0c
Olusola Alao <alaoolusolae@gmail.com>
2021-11-20 01:39:54 +0100

    update drone model

---
+++ src/main/java/musala/model/Drone.java
@@ -0,0 +1,54 @@
+package musala.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class Drone{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true, updatable = false, length = 100)
+    private String serialNumber;
+    private Emodel model;
+    private int weightLimit;
+    private int currentWeight;
+    private int batteryDischargePerDistance;
+    private int batteryCapacity;
+    private Estate state;
+
+    @ManyToOne
+    private Destination destination;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "drone_medication",
+            joinColumns = @JoinColumn(name = "drone_id"),
+            inverseJoinColumns = @JoinColumn(name = "medication_id"))
+    @ToString.Exclude
+    private List<Medication> medications = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Drone drone = (Drone) o;
+        return getSerialNumber() != null && Objects.equals(getSerialNumber(), drone.getSerialNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
