a2aeade1f04c35a1cdc0bea93c947fbaee5d256e
Olusola Alao <alaoolusolae@gmail.com>
2021-11-20 01:48:28 +0100

    update drone repository

--- src/main/java/musala/repository/DroneRepository.java
+++ src/main/java/musala/repository/DroneRepository.java
@@ -1,14 +1,16 @@
-package musala.drone.repository;
+package musala.repository;
 
-import musala.drone.model.Drone;
+import musala.model.Drone;
+import musala.model.Estate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
-public interface DroneRepository extends JpaRepository<Drone, UUID> {
+public interface DroneRepository extends JpaRepository<Drone, Long> {
     Optional <Drone> findBySerialNumber(String serialNumber);
+    Set<Drone> findAllByStateEquals(Estate idle);
 }
