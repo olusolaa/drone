bf6f7be9c0bd073c651f698e0002a848fbe2d400
Olusola Alao <alaoolusolae@gmail.com>
2021-11-20 16:15:40 +0100

    update drone service implementation

--- src/main/java/musala/service/impl/DroneServiceImpl.java
+++ src/main/java/musala/service/impl/DroneServiceImpl.java
@@ -1,8 +1,11 @@
 package musala.service.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import musala.dto.ApiResponse;
 import musala.exception.DroneNotFoundException;
+import musala.exception.MedicationNotFoundException;
+import musala.exception.WrongDestinationException;
 import musala.model.Destination;
 import musala.dto.RegisterDroneDto;
 import musala.model.Drone;
@@ -13,7 +16,6 @@ import musala.repository.DroneRepository;
 import musala.repository.MedicationRepository;
 import musala.service.DroneService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -25,6 +27,8 @@ public class DroneServiceImpl implements DroneService {
     private final DroneRepository droneRepository;
     private final MedicationRepository medicationRepository;
     private final DestinationRepository destinationRepository;
+
+    private Long droneId = 0L;
 
     @Autowired
     public DroneServiceImpl(DroneRepository droneRepository, MedicationRepository medicationRepository, DestinationRepository destinationRepository1) {
@@ -45,27 +49,32 @@ public class DroneServiceImpl implements DroneService {
         drone.setWeightLimit(droneDto.getWeightLimit());
         drone.setBatteryDischargePerDistance(droneDto.getBatteryDrainRate());
         drone.setState(Estate.IDLE);
+        log.info("Drone {} registered successfully", drone.getSerialNumber());
         return new ApiResponse<>("Drone registration successful", droneRepository.save(drone));
     }
 
     public ApiResponse<?> chargeDrone(Long droneId) {
         Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new DroneNotFoundException("Drone not found"));
+        log.info("Drone {} charging", drone.getSerialNumber());
             drone.setBatteryCapacity(100);
             droneRepository.save(drone);
-        return new ApiResponse<>("Drone " + drone.getSerialNumber() + " charged successfully", drone.getBatteryCapacity());
+            log.info("Drone {} charged successfully", drone.getSerialNumber());
+        return new ApiResponse<>("Drone " + drone.getSerialNumber() + " charged successfully", drone.getBatteryCapacity() + " %");
     }
 
     @Override
     public ApiResponse<?> unregisterDrone(Long droneId) {
-        droneRepository.deleteById(droneId);
-        return new ApiResponse<>("Drone unregistered successfully", HttpStatus.OK);
+        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new DroneNotFoundException("Drone not found"));
+        droneRepository.delete(drone);
+        log.info("Drone {} unregistered successfully", drone.getSerialNumber());
+        return new ApiResponse<>("Drone unregistered successfully", null);
     }
 
     @Override
     public ApiResponse<?> loadDrone(Long droneId, List<Medication> medications, Destination destination) {
-        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new RuntimeException("Drone not found"));;
+        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new DroneNotFoundException("Drone not found"));;
             if (drone.getDestination() != null && !Objects.equals(drone.getDestination().getCity(), destination.getCity())){
-                throw new RuntimeException("Drone " + drone.getSerialNumber() + " is already loaded with medication heading towards " + drone.getDestination().getCity());
+                throw new WrongDestinationException("Drone " + drone.getSerialNumber() + " is already loaded with medication heading towards " + drone.getDestination().getCity());
             }
             if (drone.getBatteryCapacity() < 25) {
                 throw new RuntimeException("Battery too low");
@@ -97,9 +106,9 @@ public class DroneServiceImpl implements DroneService {
 
     @Override
     public ApiResponse<?> unloadDrone(Long droneId) {
-        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new RuntimeException("Drone) not found"));
+        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new DroneNotFoundException("Drone not found"));
             if (drone.getDestination().getDistance() != 0) {
-                throw new RuntimeException("Drone is not at destination");
+                throw new WrongDestinationException("Drone is not at destination");
             }
             log.info("Drone {} arrived at destination, unloading medications", drone.getSerialNumber());
             drone.setState(Estate.DELIVERING);
@@ -112,14 +121,16 @@ public class DroneServiceImpl implements DroneService {
     }
 
     @Override
-    public ApiResponse<?> removeMedication(Long droneId, List<Medication> medications) {
+    public ApiResponse<?> removeMedications(Long droneId, List<Medication> medications) {
         Drone drone = droneRepository.findById(droneId).orElseThrow(() -> new RuntimeException("Drone not found"));
             medications.forEach(med -> {
-                        medicationRepository.findByCode(med.getCode()).ifPresent(medication -> {
-                            drone.getMedications().remove(medication);
-                            drone.setCurrentWeight(drone.getCurrentWeight() - medication.getWeight());
-                            log.info("{} removed from drone {}", medication.getName(), drone.getSerialNumber());
-                        });
+                        if (drone.getMedications().contains(med)) {
+                            drone.getMedications().remove(med);
+                            drone.setCurrentWeight(drone.getCurrentWeight() - med.getWeight());
+                            log.info("{} removed from drone {}", med.getName(), drone.getSerialNumber());
+                        } else {
+                            throw new MedicationNotFoundException("Medication not found");
+                        }
                     });
             droneRepository.save(drone);
         return new ApiResponse<>( "Medications removed successfully", drone);
@@ -139,14 +150,31 @@ public class DroneServiceImpl implements DroneService {
     }
 
     public ApiResponse<?> checkDronesAvailableForLoading() {
-        return new ApiResponse<>("Drones available for loading", droneRepository.findAllByStateEquals((Estate.IDLE)));
+        Set<Drone> drones = droneRepository.findAllByCurrentWeightEquals(0);
+        if (drones.isEmpty()) {
+            return new ApiResponse<>("No drones available for loading", null);
+        }
+        return new ApiResponse<>("Retrieved drones available for loading successfully", drones);
     }
 
     @Override
     public ApiResponse<?> chekDroneBatteryStatus(Long droneId) {
        Drone drone = droneRepository.findById(droneId).orElseThrow( () -> new DroneNotFoundException("Drone not found"));
-            drone.setBatteryCapacity(drone.getBatteryCapacity() - (drone.getDestination().getDistance() - 1) * drone.getBatteryDischargePerDistance());
+      this.droneId = droneId;
+        drone.setBatteryCapacity(drone.getBatteryCapacity() - (drone.getDestination().getDistance() - 1) * drone.getBatteryDischargePerDistance());
+        if (drone.getBatteryCapacity() <= 0) {
+            drone.setBatteryCapacity(0);
+            log.info("Drone {} battery is empty", drone.getSerialNumber());
+        }else {
             log.info("Drone {} battery status: {}", drone.getSerialNumber(), drone.getBatteryCapacity());
-          return new ApiResponse<>("Drone battery status retrieved successfully", drone.getBatteryCapacity());
+            droneRepository.save(drone);
+        }
+          return new ApiResponse<>("Drone battery status retrieved successfully", drone.getBatteryCapacity()+"%");
+    }
+
+
+    @Override
+    public Long getRecentDrone() {
+        return droneId;
     }
 }
